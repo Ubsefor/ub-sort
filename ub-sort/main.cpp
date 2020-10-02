2e4c68a1687a5cc4cf9e49f616b5ab8b65b31c4f
Ubsefor <letzdropthebas@gmail.com>
2020-10-02 17:00:59 +0300

    Initial Commit

---
+++ ub-sort/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ub-sort
+//
+//  Created by Александр Махов on 2/10/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
